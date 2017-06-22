e6b85af7a4f6ee295b6e965d9a115c2b3eb59222
LucileT <lu.troillet@gmail.com>
2017-06-22 13:12:54 +0200

    bdd

--- annonceo.sql
+++ annonceo.sql
@@ -195,3 +195,56 @@ ALTER TABLE `photo`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+INSERT INTO membre (id_membre, pseudo, mdp, nom, prenom, telephone, email, civilite, statut, date_enregistrement) VALUES
+('', 'admin', 'admin', 'admin', 'admin', '00 00 00 00 00', 'admin@hotmail.com', 'm', '1', now() ),
+('', 'Mathieu', 'ventilo', 'Moens', 'Jérôme', '06 45 75 92 15', 'je.moens@hotmail.com', 'm', '0', now() ),
+('', 'juju', 'yo', 'Paillason', 'Julien', '06 12 45 75 95', 'ju.paillason@hotmail.com', 'm', '0', now() ),
+('', 'lu', 'wouesh', 'Troillet', 'Lucile', '06 45 42 63 12', 'lu.troillet@hotmail.com', 'f', '0', now() ),
+('', 'gogo', 'golem', 'Dito', 'Flavie', '06 53 69 75 12', 'go.dito@hotmail.com', 'f', '0', now() ),
+('', 'pingu', 'mdp1', 'Duteil', 'Yves', '06 23 12 72 46', 'yves.duteil@hotmail.com', 'm', '0', now() );
+
+
+
+INSERT INTO categorie (id_categorie, titre, motscles) VALUES
+('', 'Immobilier', 'ventes immobilières, locations, colocations, bureaux et commerces'),
+('', 'Multimedia', 'informatique, consoles & jeux vidéo, image & son, téléphonie'),
+('', 'Vehicules', 'voitures, motos, caravaning, utilitaires, equipement, nautisme'),
+('', 'Loisirs', 'DVD/Films, CD/Musique, Livres, Animaux, Vélos, Sports, Collection, Jeux, Vins & Gastronomie'),
+('', 'Maison', 'ameublement, électroménager, arts de la table, décoration, linge de maison, bricolage, jardinage, vêtements, accessoires'),
+('', 'Vacances', 'locations & gîtes, chambres d\'hôtes, campings, hôtels');
+
+
+
+INSERT INTO annonce (titre, description_courte, description_longue, prix, photo, pays, ville, adresse, cp, id_membre, id_photo, id_categorie, date_enregistrement) VALUES
+
+('', 'Combiné fourmicro-ondesgrill SAMSUNG (garanti)', 'Combiné fourmicro-ondesgrill SAMSUNG (garanti)', 'Suite à un déménagement je dois me séparer de mon super four micro-ondes combiné Samsung, acheté en mars 2016. Sous garantie jusqu’à mars 2018 (la facture est fournie).
+Fonctions four, grill, micro-onde et combiné (Cuisson croustillante = très pratique pour les pizzas et quiches !)
+- Modèle : Samsung CE107MT-4B
+- Diamètre du plateau : 31,8 cm
+- Capacité : 28 l
+- Puissance micro-ondes : 900 W
+- Puissance gril : 1500 W
+- Puissance four : 2100 W (40°C à 200°C)
+- Touche de départ instantané, "touche 30s".
+- "TDS" : triple système de diffusion d\'ondes, pour une meilleure homogénéité de cuisson.
+- Plat croustilleur et programmes spécifiques spécialement dédiés à la cuisson des pizzas, tartes, volailles
+', 140, 'https://img0.leboncoin.fr/ad-large/78727fa1aeed325ce311ced85f69ad0edab03dba.jpg', 'France', 'Colombes', '24, Allée des Roses', '92700', '', '', '', now()),
+
+('', 'stickers triangle noir ', 'stickers triangle noir ', 'Grands stickers triangle à coller au mur pour une déco originale. idéal pour une chambre d\'enfant. La plaquette est composée de 52 stickers.
+Envoi par la Poste ou remise en mains propres à Bonneuil sur marne.', 5, 'https://img6.leboncoin.fr/ad-large/8f5e250266f382b415b7d6f44372651c39750d85.jpg', 'France', 'Bonneuil-sur-Marne', '77, bv Jean-Nouvel', '94380', '', '', '', now()),
+
+('Table et chaises', 'Table et chaises', 'Ensemble table plus 6 chaises . Les chaises sont en parfait état , la table a été abîmée par une bougie , d où les bandes violettes qui la recouvrent .
+Mesure de la table longueur 150 hauteur 76. Largeur 90 .
+Le tout 200Euro(s) , à retirer uniquement le 2 juillet', 200, 'https://img3.leboncoin.fr/ad-large/10f441a32b9069bdb8eb3eb3e3f1adca180fda5a.jpg', 'France', 'Paris', '4,rue du puit', '75011', '', '', '', now()),
+
+
+('Tapis akar grisblanc La Redoute', 'Tapis akar grisblanc La Redoute', 'Bonjour, Je vends ce tapis tissé à plat, Dakar gris & blanc La Redoute pour 25€ au lieu de 60€.
+Acheté il y a 6 mois donc quasi neuf. Dimensions : 120 x 170 cm. A venir chercher sur Paris 9', 25, 'https://img0.leboncoin.fr/ad-large/071f568109a007641ababb9259de8fae2ac9bada.jpg', 'France', 'Paris', '53, rue Chevreul', '75009', '', '', '', now()),
+
+('Ventilateur sur pied', 'Ventilateur sur pied', 'Vend ventilateur sur pied de marque bionaire. Très très puissant, 3 vitesses. Contact uniquement par SMS !
+A VENIR CHERCHER SUR PLACE à Maisons-Alfort.
+Ou pour voir mes autres annonces taper : Mrpaschère
+', 50, 'https://img4.leboncoin.fr/ad-large/327f148c593976a5edcf7f8c9c3c2482919b6c10.jpg', 'France', 'Maisons-Alfort', '13, rue de la Marne', '94700', '', '', '', now());
