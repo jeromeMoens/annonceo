8300cd17ca7916ca49378ef50896482277af22a2
julienpaillasson <julien.paillasson@gmail.com>
2017-06-22 11:56:05 +0200

    haut.inc
    Initialisation du fichier haut.inc.php et création du fichier fiche_annonce.php

--- inc/haut.inc.php
+++ inc/haut.inc.php
@@ -1,38 +1,39 @@
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="FR-fr">
 <head>
 	<meta charset="UTF-8">
-	<title>Mon Site</title>
-	<link rel="stylesheet" href="/php/site_boutique/inc/css/style.css">
+	<title>Annonceo</title>
+	<link rel="stylesheet" href="">
 </head>
 <body>
 
 	<header>
 		<div class="conteneur">
 			<span>
-				<a href="" title="Mon Site">MonSite.com</a>
+				<a href="" title="Annonceo">Annonceo</a>
 			</span>
 			<nav>
 				<?php	
 					if(internauteEstConnecteEtEstAdmin())
 					{// BackOffice
-						echo '<a href="' . URL . 'admin/gestion_membre.php">Gestion des membres</a>';
-						echo '<a href="' . URL . 'admin/gestion_commande.php">Gestion des commandes</a>';
-						echo '<a href="' . URL . 'admin/gestion_boutique.php">Gestion de la boutique</a>';
+						echo '<a href="' . URL . 'admin/gestion_membres.php">Gestion des membres</a>';
+						echo '<a href="' . URL . 'admin/gestion_annonces.php">Gestion des annonces</a>';
+						echo '<a href="' . URL . 'admin/gestion_categories.php">Gestion des catégories</a>';
+						echo '<a href="' . URL . 'admin/gestion_commentaires.php">Gestion des commentaires</a>';
+						echo '<a href="' . URL . 'admin/gestion_notes.php">Gestion des notes</a>';
+						echo '<a href="' . URL . 'admin/statistiques.php">Statistiques</a>';
 					}
 					if(internauteEstConnecte())// membre du site ayant un compte
 					{
 						echo '<a href="' . URL . 'profil.php">Voir votre profil</a>';
-						echo '<a href="' . URL . 'boutique.php">Accés à la boutique</a>';
-						echo '<a href="' . URL . 'panier.php">Voir votre panier</a>';
+						echo '<a href="' . URL . 'annonces.php">Voir les annonces</a>';
 						echo '<a href="' . URL . 'connexion.php?action=deconnexion">Se déconnecter</a>';
 					}
 					else// visiteur
 					{
 						echo '<a href="' . URL . 'inscription.php">Inscription</a>';
-						echo '<a href="' . URL . 'boutique.php">Accés à la boutique</a>';
-						echo '<a href="' . URL . 'panier.php">Voir votre panier</a>';
+						echo '<a href="' . URL . 'annonces.php">Voir les annonces</a>';
 						echo '<a href="' . URL . 'connexion.php">Connexion</a>';
 					}
 					
